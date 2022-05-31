51ab85d99c8c201fdf9382910efe33389b2a7e64
dd-code-immersives <domilamad89@gmail.com>
2022-05-31 17:54:28 +0200

    updated django tests

--- geolocation/tests/test_views.py
+++ geolocation/tests/test_views.py
@@ -3,29 +3,29 @@ from django.test import RequestFactory
 from django.urls import reverse
 from geolocation import views
 
-# class TestIndexView:
-#   """
-#   It's optional to create a class, but it helps to group tests into suites
-#   """
-#     def test_index(self):
-#         req = RequestFactory().get(reverse("geolocation:index"))
-#         resp = views.MyView.as_view()(req)
-#         assert resp.status_code == 200
+class TestIndexView:
+    """
+    It's optional to create a class, but it helps to group tests into suites
+    """
+    def test_index(self):
+        req = RequestFactory().get(reverse("index"))
+        resp = views.index(req)
+        assert resp.status_code == 200
 
 
 class TestIssView:
 
     def test_iss_people(self):
-        req = RequestFactory().get(reverse("geolocation:iss_people"))
-        resp = views.iss_people.as_view()(req)
-        assert resp.status_code == 200
+        req = RequestFactory().get(reverse("iss_people"))
+        resp = views.iss_people(req)
+        assert resp.status_code == 200 
 
     def test_iss_location(self):
-        req = RequestFactory().get(reverse("geolocation:iss_location"))
-        resp = views.iss_location.as_view()(req)
+        req = RequestFactory().get(reverse("iss_location"))
+        resp = views.iss_location(req)
         assert resp.status_code == 200
 
     def test_iss_info(self):
-        req = RequestFactory().get(reverse("geolocation:iss_info"))
-        resp = views.iss_info.as_view()(req)
+        req = RequestFactory().get(reverse("iss_info"))
+        resp = views.iss_info(req)
         assert resp.status_code == 200
