61401d6a1d43f04b5daac05f7bde772ba522b039
dd-code-immersives <domilamad89@gmail.com>
2022-06-15 17:03:39 +0200

    refactored views to be class based

--- geolocation/urls.py
+++ geolocation/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('iss_people', views.iss_people, name='iss_people'),
-    path('iss_location', views.iss_location, name='iss_location'),
-    path('iss_info', views.iss_info, name='iss_info'),
-    path('get_time', views.get_time, name='time_'),
+    path('', views.Index.as_view(), name='index'),
+    path('iss_people', views.ISSPeople.as_view(), name='iss_people'),
+    path('iss_location', views.ISSLocation.as_view(), name='iss_location'),
+    path('iss_info', views.ISSInfo.as_view(), name='iss_info'),
+    path('get_time', views.Time.as_view(), name='time_'),
 
 ]
--- geolocation/views.py
+++ geolocation/views.py
@@ -1,40 +1,53 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from django.views import View
 from .forms import AddressForm
 from .utils import *
 
-def index(request):
-    
-    if request.method == 'POST':
-        form = AddressForm(request.POST)
+class Index(View):
 
+    def get(self, request, *args, **kwargs):
+        form = AddressForm()
+        return render(request, 'geolocation/index.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = AddressForm(request.POST)
         if form.is_valid():
             addy = form.cleaned_data['address']
             res = get_coordinates(addy)
             return render(request, 'geolocation/results.html', res)
-    else:
-    	form = AddressForm()
-    return render(request, 'geolocation/index.html', {'form': form})
-
-def iss_people(request):
-	return JsonResponse(get_iss_people())
-
-def iss_location(request):
-	return JsonResponse(get_iss_location())
-
-def iss_info(request):
-	
-	people = get_iss_people()
-	location = get_iss_location()
-	people.update(location)
-	return JsonResponse(people)
+        else:
+            return render(request, 'geolocation/index.html', {'form': form})
 
 
-def get_time(request):
+class ISSPeople(View):
 
-    time_now = get_time_now()
-    res = {'time': time_now}
-    return JsonResponse(res)
+    def get(self, request, *args, **kwargs):
+
+        return JsonResponse(get_iss_people())
 
 
+class ISSLocation(View):
+
+    def get(self, request, *args, **kwargs):
+        
+        return JsonResponse(get_iss_location())
+
+
+
+class ISSInfo(View):
+
+    def get(self, request, *args, **kwargs):
+        people = get_iss_people()
+        location = get_iss_location()
+        people.update(location)
+        return JsonResponse(people)
+
+
+class Time(View):
+
+    def get(self, request, *args, **kwargs):	
+        time_now = get_time_now()
+        res = {'time': time_now}
+        return JsonResponse(res)
 
